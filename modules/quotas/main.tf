9d1f8b362fdba1b4e156b3fdb88e5e4e08300d90
shemau <shemau@gmail.com>
2025-08-19 16:43:35 +0100

    fix: id_attribute to circumvent idempotent issues

--- modules/quotas/main.tf
+++ modules/quotas/main.tf
@@ -9,6 +9,7 @@ resource "restapi_object" "container_registry_storage_quota" {
   data = jsonencode({
     "storage_megabytes" : var.storage_megabytes
   })
+  id_attribute   = "storage_megabytes"
   create_method  = "PATCH"
   create_path    = "//${var.container_registry_endpoint}/api/v1/quotas"
   destroy_method = "PATCH"
@@ -30,6 +31,7 @@ resource "restapi_object" "container_registry_traffic_quota" {
   data = jsonencode({
     "traffic_megabytes" : var.traffic_megabytes
   })
+  id_attribute   = "traffic_megabytes"
   create_method  = "PATCH"
   create_path    = "//${var.container_registry_endpoint}/api/v1/quotas"
   destroy_method = "PATCH"
